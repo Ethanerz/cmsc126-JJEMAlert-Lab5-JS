1d8b93c70a6eca44088e6eca744d89979360ef82
Ethanerz <ethangapulan@gmail.com>
2026-03-20 15:15:02 +0800

    added user input handling

--- lab5_scripts.js
+++ lab5_scripts.js
@@ -1,3 +1,5 @@
+const students = [];
+
 function time_now(){
     const now = new Date();
     const options ={
@@ -12,18 +14,42 @@ function time_now(){
     document.getElementById("output").innerHTML = 'Today is ' + now.toLocaleDateString('en-US', options) + "<br>The current time is " + now.toLocaleTimeString('en-US',optionsTime);
 }
 
-class Student{
-    constructor(name, age, email, course){
-        this.studentNo = "2024" + numGen();
-        this.name = nameVer(name);
-        this.age = ageVer(age);
-        this.email = emailVer(email);
-        this.course = courseVer(course);
+function numGen(){
+        let num;
+		do {
+			num = "2024" + (Math.floor(Math.random() * 90000) + 10000);
+		} while (students.some(s => s.studentNo === num));
+		document.getElementById("snum").value = num;
+		return num;
+    }
 
+function add_student() {
+    const studentNo = document.getElementById("snum").value;
+    const name = document.getElementById("uname").value;
+    const age = Number(document.getElementById("age").value);
+    const email = document.getElementById("mail").value;
+    const course = document.getElementById("course").value;
+
+    const student = new Student(studentNo, name, age, email, course);
+    students.push(student);
+    console.log("Student added!", students);
+
+    document.getElementById("snum").value = "";
+    document.getElementById("uname").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("mail").value = "";
+    document.getElementById("course").selectedIndex = 0;
+}
+
+class Student {
+    constructor(studentNo, name, age, email, course) {
+        this.studentNo = studentNo;
+        this.name = this.nameVer(name);
+        this.age = this.ageVer(age);
+        this.email = this.emailVer(email);
+        this.course = course;
     }
-    numGen(){
-        return Math.floor(Math.random()* 90000) + 10000
-    }
+    
     nameVer(name){
          if (name.length > 5 && name.includes(" ")) {
             return name;
